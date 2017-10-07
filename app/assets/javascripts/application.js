2212e3be5cb580d3d5e416cd508d542073b33620
1412393 <1412393@student.hcmus.edu.vn>
2017-10-07 20:52:28 +0700

    thêm chức năng xóa  blog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,56 +1,56 @@
 class BlogsController < ApplicationController
 
-    before_action :get_blog, only: [:show,:edit, :update, :destroy]
+  before_action :get_blog, only: [:show,:edit, :update, :destroy]
 
-    def index
-        @blogs=Blog.all
-    end
+  def index
+    @blogs=Blog.all
+  end
 
-    def create
-      @blog = Blog.new(title: params[:post][:title],  body: params[:post][:body], user_id: 1, view: 0 )
-      if @blog.save
+  def create
+    @blog = Blog.new(title: params[:post][:title],  body: params[:post][:body], user_id: 1, view: 0 )
+    if @blog.save
       flash[:success] = "Create successfully."
       render :show
     end
+  end
 
-    def show
-       #@blog.view=1
-       #@blog.save
-      # @blog = Blog.find(params[:id])
-       @blog.update(view: @blog.view+1) 
-       @blog.save
-    end
+  def show
+    #@blog.view=1
+    #@blog.save
+    # @blog = Blog.find(params[:id])
+    @blog.update(view: @blog.view+1) 
+    @blog.save
+  end
 
-    def update
-      if @blog.update(blog_params)
-        flash[:success] = "Update successfully."
-        redirect_to blog_path(@blog.id)
-      else
-        flash[:error] = "Cannot update."
-        render :show
-      end
+  def update
+    if @blog.update(blog_params)
+      flash[:success] = "Update successfully."
+      redirect_to blog_path(@blog.id)
+    else
+      flash[:error] = "Cannot update."
+      render :show
     end
+  end
   
-    def destroy
-      
-  
-      begin
-        @blog.destroy!
-        flash[:success] = "Delete successfully."
-        redirect_to blogs_path
-      rescue ActiveRecord::RecordNotDestroyed => e
-        flash[:error] = "Cannot delete."
-        render :show
-      end
+  def destroy
+    begin
+      @blog.destroy!
+      flash[:success] = "Delete successfully."
+      redirect_to blogs_path
+    rescue ActiveRecord::RecordNotDestroyed => e
+      flash[:error] = "Cannot delete."
+      render :show
     end
+  end
 
 
-    private
-    def get_blog
-      @blog = Blog.find(params[:id])
-    end
+  private
+  def get_blog
+    @blog = Blog.find(params[:id])
+  end
   
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
+
