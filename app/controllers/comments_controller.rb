9c8277e7c6ec89d9efacedbe388f4fd3afb33558
1412393 <1412393@student.hcmus.edu.vn>
2017-10-08 19:49:21 +0700

    thêm chức năng post comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,11 @@ class CommentsController < ApplicationController
     end
             
     def create
-        
+        @comment = Comment.new(body: params[:post][:body], user_id: params[:post][:user_id], blog_id: params[:post][:blog_id] )
+        if @comment.save
+          flash[:success] = "Comment successfully."
+          redirect_to blog_path(@comment.blog_id)
+        end
     end
 
 end
